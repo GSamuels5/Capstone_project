43281a9bb037df7badcf08d4b427bcf9ef381adc
GSamuels5 <ghamzahsamuels23@gmail.com>
2024-03-11 11:55:57 +0200

    vue project created

---
+++ middleware/Errorhandling.js
@@ -0,0 +1,17 @@
+function errorHandling(err, req, res, next) {
+    console.log(res)
+    if (err || res.statusCode >= 400) {
+
+        res.json({
+            status: err.status || res.statusCode || 500,
+            msg: 'An error occured. Please try again later'
+        })
+
+    }else{
+        next()
+    }
+    
+}
+export{
+    errorHandling
+}
