50cbc710c6da0c7d8b6fe23a3e283371da657ea7
GSamuels5 <ghamzahsamuels23@gmail.com>
2024-03-12 14:47:45 +0200

    spinner component

--- Frontend/capstone_frontend/src/store/index.js
+++ Frontend/capstone_frontend/src/store/index.js
@@ -2,6 +2,11 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    workers: null,
+    worker:null,
+    pay: null,
+    paid:null,
+    days: null
   },
   getters: {
   },
