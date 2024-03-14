f258aa76c504d98f4ad2d5ce5dfe00a4fd0d80ee
GSamuels5 <ghamzahsamuels23@gmail.com>
2024-03-14 12:52:19 +0200

    new icons and form link

--- capstone_frontend/src/views/ContactView.vue
+++ capstone_frontend/src/views/ContactView.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>
+    <div class="vh-100">
         <div>
 
 <h1>Contact us</h1>
         </div>
-        <div class="formation align-content-sm-center justify-content-center bg-danger text-white">
-  <form action="https://formspree.io/f/xbjnljop" method="post">
+        <div class="formation align-content-sm-center justify-content-center  text-white">
+  <form action="https://formspree.io/f/xqkrlond" method="post">
                 <div class="forming row col-12 m-0">
                     <label for="firstname">First name:</label>
                     <input type="text" id="firstname" placeholder="enter your name" required >
