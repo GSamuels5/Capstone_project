80c366412cf07816bc36de8a63c6264ebf259598
GSamuels5 <ghamzahsamuels23@gmail.com>
2024-03-22 12:35:20 +0200

    done working on log out

--- capstone_frontend/src/views/EmployeeView.vue
+++ capstone_frontend/src/views/EmployeeView.vue
@@ -1,14 +1,30 @@
 <template>
     <div>
 <h1>Employee</h1>
+<div v-if="loggedInUser">
+    <p>Welcome {{loggedInUser.firstName}} {{loggedInUser.surname}}</p>
+    <div><img :src="loggedInUser.empImg" ></div>
+
+</div>
+<div v-else>
+    <p>No user logged in</p>
+
+</div>
     </div>
 </template>
 
 <script>
+import {useCookies} from "vue3-cookies"
 
+const {cookies} = useCookies();
 
 export default {
-
+computed:{
+    loggedInUser(){
+        const userData = cookies.get('Worker')?.result
+        return userData 
+    }
+}
 }
 </script>
 
